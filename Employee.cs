60bb78f1ce267713416df59feed5605732ca989b
gai52155 <gai52155@outlook.com>
2017-03-20 21:34:37 +0700

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace gridTableTest
+{
+    public class Employee
+    {
+        public Employee(String name, String companyName, String title)
+        {
+            this.Name = name;
+            this.CompanyName = companyName;
+            this.Title = title;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string CompanyName
+        {
+            get;
+            set;
+        }
+        public string Title
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace gridTableTest
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PopulateData();
+        }
+
+        public void PopulateData()
+        {
+            ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            employees.Add(new Employee("andre aaa", "EiEi", "Sale"));
+            this.radDataPager.Source = employees;
+        }
+    }
+}
